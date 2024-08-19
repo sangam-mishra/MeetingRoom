5a13d0c64b5f27fddfe536faf1fc247e4ff7e994
Karamveer Singh <zaildarsingh26@gmail.com>
2024-08-19 09:10:19 +0530

    Admin Page

---
+++ src/main/webapp/static/js/scripts.js
@@ -0,0 +1,19 @@
+// scripts.js
+
+function createRoom() {
+    alert("Redirect to Create Room Page");
+    // Here, you can redirect to the create room page
+    window.location.href = 'createRoom.html';
+}
+
+function editRooms() {
+    alert("Redirect to Edit Rooms Page");
+    // Here, you can redirect to the edit rooms page
+    window.location.href = 'editRooms.html';
+}
+
+function seeUsers() {
+    alert("Redirect to See Users Page");
+    // Here, you can redirect to the see users page
+    window.location.href = 'seeUsers.html';
+}
